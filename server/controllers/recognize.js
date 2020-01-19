13303ce26c24646e32f4bd47459d27c9d16573bb
Siusarna <42269105+Siusarna@users.noreply.github.com>
2020-01-19 19:54:59 +0200

    Add endpoint for recognize by humming

--- server/controllers/recognize.js
+++ server/controllers/recognize.js
@@ -1,9 +1,8 @@
 const request = require ('request-promise-native');
 const fs = require ('fs');
 
-const makeRequest = filename => {
+const makeRequestToAudd = (filename, uri) => {
     const pathToFile = './uploads/' + filename;
-
     const data = {
         'file': fs.createReadStream (pathToFile),
         'return': 'deezer',
@@ -11,24 +10,94 @@ const makeRequest = filename => {
     };
 
     const options = {
-        uri: 'https://api.audd.io/',
+        uri,
         formData: data,
         method: 'POST'
     };
 
-    return request(options)
+    return request (options);
+};
+
+const parseAnswerWithPreview = (result) => {
+    return {
+        artist: result.result.artist,
+        title: result.result.title,
+        preview: result.result.deezer.preview
+    };
 };
 
 const searchMusicByUserFile = (req, res) => {
-    makeRequest(req.file.originalname)
+    const uri = 'https://api.audd.io/';
+    makeRequestToAudd (req.file.originalname, uri)
         .then ((result) => {
-            res.json (JSON.parse(result));
+            const parsedResult = parseAnswerWithPreview (JSON.parse (result));
+            res.json (parsedResult);
         })
         .catch (err => {
-            res.status (400).json ({message: err});
+            res.status (400).json ({message: err.message});
         });
 };
 
+const makeRequestToDeezer = async (title, artist) => {
+    const uri = `https://api.deezer.com/search?q=title:"${title}" artist:"${artist}"`;
+    console.log (uri);
+    const options = {
+        uri,
+        method: 'GET'
+    };
+    return await request (options);
+};
+
+const searchPreviewInDeezerByTitle = (title, arrWithOnlyOneAnswer) => {
+    let i = 0;
+    while (arrWithOnlyOneAnswer[i].title.toLowerCase () !== title.toLowerCase () && i < arrWithOnlyOneAnswer.length - 1) {
+        i++;
+    }
+    if (arrWithOnlyOneAnswer[i].title.toLowerCase () === title.toLowerCase ()) {
+        return arrWithOnlyOneAnswer[i].preview;
+    }
+};
+
+const makeRequestAndSearchPreviewInAnswer = async element => {
+    const {title, artist} = element;
+    const result = await makeRequestToDeezer (title, artist);
+    return searchPreviewInDeezerByTitle (title, JSON.parse (result).data);
+};
+
+
+const parseAnswerForRecognizeHumming = async (result) => {
+    const arrayOfResult = [];
+    if(!result.result){
+        return;
+    }
+    for (const obj of result.result.list) {
+        const element = {
+            artist: obj.artist,
+            title: obj.title
+        };
+        element.preview = await makeRequestAndSearchPreviewInAnswer (element);
+        arrayOfResult.push (element);
+    }
+    return arrayOfResult;
+};
+
+const searchMusicByHumming = (req, res) => {
+    const uri = 'https://api.audd.io/recognizeWithOffset/';
+    makeRequestToAudd (req.file.originalname, uri)
+        .then ((result) => {
+            return parseAnswerForRecognizeHumming (JSON.parse (result));
+        })
+        .then ((parsedResult) => {
+            res.json (parsedResult);
+        })
+        .catch (err => {
+            console.log (err);
+            res.status (400).json ({message: err.message});
+        });
+
+};
+
 module.exports = {
-    searchMusicByUserFile
+    searchMusicByUserFile,
+    searchMusicByHumming
 };
--- server/routers/recognize.js
+++ server/routers/recognize.js
@@ -19,4 +19,8 @@ const upload = multer ({
 
 module.exports = app => {
     app.post ('/api/recognize', upload.single ('recognize'), controllerForRecognize.searchMusicByUserFile);
+    app.post ('/api/recognizeByHumming', upload.single ('recognize'), controllerForRecognize.searchMusicByHumming);
+    app.get ('/api', (req, res) => {
+        res.sendFile (__dirname + '/index.html');
+    });
 };
