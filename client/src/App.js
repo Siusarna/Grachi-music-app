3f93f09163c5536a58917e83d748aef4dd361527
Zakkaray <bloomzakk@gmail.com>
2020-01-19 03:21:30 +0200

    Starter page ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./stylesheets/app.css";
+import Intro from "./Components/Intro";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cover-full">
+      <Intro className="" />
     </div>
   );
-}
+};
 
 export default App;
