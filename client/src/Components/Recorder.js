0904f5213cdeaa1623848d9a35de6eb2cebf901d
Zakkaray <bloomzakk@gmail.com>
2020-01-21 02:51:36 +0200

    block fix

--- client/src/Components/Recorder.js
+++ client/src/Components/Recorder.js
@@ -8,42 +8,19 @@ const Recorder = () => {
   const [isRecording, setRecording] = useState(false);
   const [isBlocked, setBlocked] = useState(false);
   const [Recorded, setRecorded] = useState('');
-  const start = () => {
-    if (isBlocked) {
-      console.log("Permission Denied");
-    } else {
-      Mp3Recorder.start()
-        .then(() => {
-          setRecording(true);
-        })
-        .catch(e => console.error(e));
-    }
-  };
-  useEffect(() => {
-    navigator.getUserMedia(
-      { audio: true },
-      () => {
-        console.log("Permission Granted");
-        setBlocked(false);
-      },
-      () => {
-        console.log("Permission Denied");
-        setBlocked(true);
-      }
-    );
-  });
   const onStop = recordedBlob => {
     setRecorded(recordedBlob);
   };
   return (
     <>
-    <MDBCol lg={7} className="align-self-center">
+    <MDBCol xs={1} className="align-self-center">
       <ReactMic
         record={isRecording}
         className="sound-wave normalize-width"
         onStop={onStop}
         strokeColor="#FFF"
         backgroundColor="#3a3a3a"
+        width={'300'}
       />
       </MDBCol>
       <MDBRow>
@@ -54,17 +31,14 @@ const Recorder = () => {
       >
         <h4 className="mb-0">{isRecording ? "Stop" : "Record"}</h4>
       </MDBBtn>
-      {Recorded ? (
         <MDBBtn
           className="stable-width mx-auto"
           color="elegant"
           onClick={e => setRecording(!isRecording)}
+          disabled={isRecording || !Recorded ? true : false}
         >
           <h4 className="mb-0">Finish</h4>
         </MDBBtn>
-      ) : (
-        ""
-      )}
       </MDBRow>
     </>
   );
