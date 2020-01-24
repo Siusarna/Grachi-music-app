a2fa71bf2cee54d222012a54704f7c33a2d96927
roflantohan <anton.bezz.v@gmail.com>
2020-01-24 20:59:09 +0200

    fix merge

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,11 +1,8 @@
 const test = require('../controllers/test');
 const routeForSearchByRecognize = require('./recognize');
 const searchByText = require('./searchByText');
-
 module.exports = app => {
     app.get ('/', test.test);
     routeForSearchByRecognize(app);
-};
-module.exports = app => {
     searchByText(app);
 };
