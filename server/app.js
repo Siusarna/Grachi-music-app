978de301dc89263211de481ca777b35f71f86b53
roflantohan <47608077+roflantohan@users.noreply.github.com>
2020-01-24 20:57:24 +0200

    app.use(express.json()); by roflantohan

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const fingerprint = require('express-fingerprint');
 
 const app = express ();
+app.use(express.json());
 app.use (fingerprint());
 require ('./routers/index') (app);
 
