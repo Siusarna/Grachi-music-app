22e5c06e907dbe8260c5c25d690e59954e734c3c
roflantohan <anton.bezz.v@gmail.com>
2020-01-24 19:57:14 +0200

    fix conflicts merge

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const fingerprint = require('express-fingerprint');
 const app = express ();
 app.use(express.json());
 app.use(fingerprint());
-require ('./routes/index') (app);
+require ('./routergs/index') (app);
 
 
 const PORT = process.env.PORT || 3000;
